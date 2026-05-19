d17dc3876d9f98d7b3b5315600269cd3d502e02a
Herapjs <Polaroidploy2538@gmail.com>
2026-05-19 15:37:44 +0700

    addd

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,13 +2,24 @@ import "dotenv/config"; //ทำหน้าที่โหลดค่าตั
 import { PrismaMariaDb } from "@prisma/adapter-mariadb"; //ตัวช่วยจัดการการเชื่อมต่อ (Adapter) เฉพาะสำหรับ MariaDB
 import { PrismaClient } from "../generated/prisma/client.js"; //ตัวหลักของ Prisma ที่เราจะใช้เขียนคำสั่งจัดการข้อมูล (เช่น prisma.user.findMany())
 
+// const adapter = new PrismaMariaDb({
+//  host: process.env.DATABASE_HOST,
+//  user: process.env.DATABASE_USER,
+//  password: process.env.DATABASE_PASSWORD,
+//  database: process.env.DATABASE_NAME,
+//  connectionLimit: 5, //เป็นการจำกัดว่าให้เปิดท่อเชื่อมต่อค้างไว้สูงสุดได้ 5 ท่อ พร้อมกัน ช่วยป้องกันไม่ให้แอปพลิเคชันแย่งทรัพยากรฐานข้อมูลจนเกินไปจนระบบล่ม
+// });
+
+const databaseUrl = new URL(process.env.DATABASE_URL);
+
 const adapter = new PrismaMariaDb({
- host: process.env.DATABASE_HOST,
- user: process.env.DATABASE_USER,
- password: process.env.DATABASE_PASSWORD,
- database: process.env.DATABASE_NAME,
- connectionLimit: 5, //เป็นการจำกัดว่าให้เปิดท่อเชื่อมต่อค้างไว้สูงสุดได้ 5 ท่อ พร้อมกัน ช่วยป้องกันไม่ให้แอปพลิเคชันแย่งทรัพยากรฐานข้อมูลจนเกินไปจนระบบล่ม
-});
+    host: databaseUrl.hostname,
+    port: parseInt(databaseUrl.port) || 3306,
+    user: decodeURIComponent(databaseUrl.username),
+    password: decodeURIComponent(databaseUrl.password),
+    database: databaseUrl.pathname.slice(1),
+    connectionLimit: 20
+})
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
